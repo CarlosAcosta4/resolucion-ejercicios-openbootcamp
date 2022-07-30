ffb5075e34d5ec64b8b589f8b84c3f208c17e011
CarlosAcosta4 <carlosacosta4820@gmail.com>
2022-07-30 18:05:15 -0500

    Subiendo los Ejercicios

---
+++ Curso Python/Tema 2/ejercicio1.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls')
+
+texto = 'Hola mundo'
+
+print(texto)
+
+print(type(texto))
---
+++ Curso Python/Tema 2/ejercicio2.py
@@ -0,0 +1,9 @@
+import os
+os.system('cls')
+
+texto = 'Hola mundo'
+
+texto = 'Hola mundo , mi nombre es carlos'
+
+print(texto )
+
