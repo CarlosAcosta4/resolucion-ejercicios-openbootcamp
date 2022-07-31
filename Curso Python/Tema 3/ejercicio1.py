77f3b77762b5b14f23000db3bf3c3654cbd93021
CarlosAcosta4 <carlosacosta4820@gmail.com>
2022-07-31 00:56:49 -0500

    Subiendo ejercicio 1 - tema 3 - modulo Python

---
+++ Curso Python/Tema 3/ejercicio1.py
@@ -0,0 +1,14 @@
+import os
+os.system('cls')
+
+nombre = 'Carlos'
+apellido = 'Acosta'
+edad = 21
+email = 'carlosacosta4820@gmail.com'
+telefono = 999777888
+casado = 'falso'
+hijos = 'falso'
+listaAmigos = ['Jose', 'Martha', 'Javier', 'Mario']
+peliculasVistas = { 'v1' : 'Titanic' , 'v2' : 'Deadpool' , 'v3' : 'mazeRunner' , 'v4' : 'Joker'}
+
+print(nombre, '\n',apellido,'\n',edad,'\n',email,'\n',telefono,'\n',casado,'\n',hijos,'\n',listaAmigos,'\n',peliculasVistas)
